43e988f53f5d56d76265cca0cc00c61df15e0e63
丁敬 <dingjing@live.cn>
2019-05-24 20:12:25 +0800

    flux

--- react/flux-demo/src/view/Summary.js
+++ react/flux-demo/src/view/Summary.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-
 import SummaryStore from '../SummaryStore.js';
 
 class Summary extends Component {
-
     constructor(props) {
         super(props);
-
         this.onUpdate = this.onUpdate.bind(this);
-
         this.state = {
             sum: SummaryStore.getSummary()
         }
     }
-
     componentDidMount() {
         SummaryStore.addChangeListener(this.onUpdate);
     }
@@ -21,19 +16,16 @@ class Summary extends Component {
     componentWillUnmount() {
         SummaryStore.removeChangeListener(this.onUpdate);
     }
-
     onUpdate() {
         this.setState({
             sum: SummaryStore.getSummary()
         })
     }
-
     render() {
         return (
             <div>Total Count: {this.state.sum}</div>
         );
     }
 }
-
 export default Summary;
 
