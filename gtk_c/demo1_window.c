f93c398cab8c9b307db93fa69454588bac23f3b5
dingjingmaster <dingjing@live.cn>
2019-10-08 13:18:40 +0800

    change

--- gtk_c/demo1_window.c
+++ gtk_c/demo1_window.c
@@ -9,28 +9,7 @@ extern "C" {
 }
 #endif
 
-//int main(int argc, char* argv[])
-//{
-//   GtkWidget* window;
-//   GtkWidget* label;
-
-//   gtk_init(&argc, &argv);
-
-//   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-
-//   label = gtk_label_new("Hello World.");
-
-//   gtk_container_add(GTK_CONTAINER(window), label);
-
-//   gtk_widget_show_all(window);
-
-//   gtk_main();
-
-//   return 0;
-//}
-
-static void
-activate (GtkApplication* app, gpointer user_data) {
+static void activate (GtkApplication* app, gpointer user_data) {
   GtkWidget *window;
 
   window = gtk_application_window_new (app);
