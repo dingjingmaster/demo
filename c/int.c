2e5dabd2532275a408a1ecbe1deee8e01ff1429e
dingjingmaster <dingjing@live.cn>
2022-08-14 15:11:13 +0800

    int

---
+++ c/int.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+> FileName: int.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Sun 14 Aug 2022 01:52:46 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main ()
+{
+    printf ("%d\n", (int) - 1);
+
+    return 0;
+}
