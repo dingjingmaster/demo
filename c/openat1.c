574d5a58ad6f10b133345e9b84e2284eca1627e7
dingjingmaster <dingjing@live.cn>
2022-12-19 14:17:14 +0800

    暂存

--- c/openat1.c
+++ c/openat1.c
@@ -4,27 +4,50 @@
 > Mail    : dingjing@live.cn
 > Created Time: Mon 19 Dec 2022 09:53:54 AM CST
  ************************************************************************/
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
 
 int main (int argc, char* argv[])
 {
-    int fd = 0;
-    int n = 0;
-
-    fd = openat (AT_FDCWD, "test.log", O_CREAT | O_RDWR | O_EXCL, 0777);
-    if (-1 == fd) {
+    //int fd = openat (AT_FDCWD, "test.log", O_WRONLY | O_RDONLY | O_CREAT /*| O_EXCL*/, 0775);
+    int fr = openat (AT_FDCWD, "test.log", O_RDWR | O_CREAT /*| O_EXCL*/, 0775);
+    if (-1 == fr) {
         perror ("openat error");
         return -1;
     }
 
-    n = write (fd, "hello world!\n", 12);
+    write (fr, "hello world!\n", 14);
+    fsync (fr);
+    lseek (fr, 0, SEEK_SET);
 
-    close (fd);
+    // 写
+    int fw = openat (AT_FDCWD, "test-copy.log", O_RDWR | O_CREAT /*| O_EXCL*/, 0775);
+    if (-1 == fw) {
+        perror ("openat error");
+        return -1;
+    }
+
+    //ioctl (fw, BTRFS_IOC_CLONE, fr);
+
+    char buf[1024] = {0};
+
+    int ret = read (fr, buf, sizeof(buf) - 1);
+    if (ret < 0) {
+        printf ("%d -- %d -- %d -- %s\n", fr, EBADF, errno, strerror(errno));
+        return -1;
+    }
+
+
+    printf ("\nread: %s\n", buf);
+
+    close (fr);
 
     return 0;
 }
---
+++ c/pipe2.c
@@ -0,0 +1,46 @@
+/*************************************************************************
+> FileName: pipe2.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Mon 19 Dec 2022 11:18:19 AM CST
+ ************************************************************************/
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define F_LINUX_SPECIFIC_BASE               1024
+
+#ifndef F_SETPIPE_SZ
+#define F_SETPIPE_SZ (F_LINUX_SPECIFIC_BASE + 7)
+#endif
+
+int main (int argc, char* argv[])
+{
+    // array of 2 size a[0] is for reading 
+    // and a[1] is for writing over a pipe 
+    int a[2] = {0};
+
+    // opening of pipe using pipe(a)   
+    char buff[10] = {0};
+    if (pipe2(a, O_CLOEXEC) == -1) {
+        perror("pipe"); // error in pipe
+        exit(1); // exit from the program
+    }
+
+    fcntl(a[1], F_SETPIPE_SZ, 1048576);
+
+    // writing a string "code" in pipe
+    write(a[1], "code", 5);
+    printf("\n");
+
+    // reading pipe now buff is equal to "code"
+    read(a[0], buff, 5);
+
+    // it will print "code"
+    printf("%s", buff);
+
+    return 0;
+}
