9f56efc3d7b0a59920cf505d241819959397b339
dingjingmaster <dingjing@live.cn>
2023-01-11 18:09:33 +0800

    glib

---
+++ c/left-move.c
@@ -0,0 +1,36 @@
+/*************************************************************************
+> FileName: left-move.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Wed 11 Jan 2023 05:13:24 PM CST
+ ************************************************************************/
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+    printf ("(255 << 2) -- %lu\n", (unsigned long) (255 << 2));
+    printf ("(1 << 2) - 1 -- %lu\n", (unsigned long) (1 << 2 - 1));
+    printf ("(1 << 2) -- %lu\n", (unsigned long) (1 << 2));
+    printf ("(2 << 2) -- %lu\n", (unsigned long) (2 << 2));
+    printf ("(3 << 2) -- %lu\n", (unsigned long) (3 << 2));
+    printf ("(4 << 2) -- %lu\n", (unsigned long) (4 << 2));
+    printf ("(5 << 2) -- %lu\n", (unsigned long) (5 << 2));
+    printf ("(6 << 2) -- %lu\n", (unsigned long) (6 << 2));
+    printf ("(7 << 2) -- %lu\n", (unsigned long) (7 << 2));
+    printf ("(8 << 2) -- %lu\n", (unsigned long) (8 << 2));
+    printf ("(9 << 2) -- %lu\n", (unsigned long) (9 << 2));
+    printf ("(10 << 2) -- %lu\n", (unsigned long) (10 << 2));
+    printf ("(11 << 2) -- %lu\n", (unsigned long) (11 << 2));
+    printf ("(12 << 2) -- %lu\n", (unsigned long) (12 << 2));
+    printf ("(13 << 2) -- %lu\n", (unsigned long) (13 << 2));
+    printf ("(14 << 2) -- %lu\n", (unsigned long) (14 << 2));
+    printf ("(15 << 2) -- %lu\n", (unsigned long) (15 << 2));
+    printf ("(16 << 2) -- %lu\n", (unsigned long) (16 << 2));
+    printf ("(17 << 2) -- %lu\n", (unsigned long) (17 << 2));
+    printf ("(18 << 2) -- %lu\n", (unsigned long) (18 << 2));
+    printf ("(19 << 2) -- %lu\n", (unsigned long) (19 << 2));
+    printf ("(20 << 2) -- %lu\n", (unsigned long) (20 << 2));
+    printf ("(21 << 2) -- %lu\n", (unsigned long) (21 << 2));
+
+    return 0;
+}
