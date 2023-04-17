bf5a3595efc339b79e34bd260427abdd56c93738
dingjingmaster <dingjing@live.cn>
2023-04-17 09:55:46 +0800

    zc

---
+++ c/uint_255_zy_2.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+> FileName: uint_255_zy_2.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Fri 14 Apr 2023 01:05:33 PM CST
+ ************************************************************************/
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("255 << 2: %lu\n", (unsigned long)(255 << 2));
+    printf ("1 << 2 - 1: %lu\n", (unsigned long)(1 << 2) - 1);
+
+    return 0;
+}
---
+++ glib/glib-g_intern_static_string.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+> FileName: glib-g_intern_static_string.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Fri 14 Apr 2023 05:34:49 PM CST
+ ************************************************************************/
+#include <glib.h>
+
+int main (int argc, char* argv[])
+{
+    gint a = 0;
+
+    printf ("GTypeInfo: %s\n", g_intern_static_string("GTypeInfo"));
+    printf ("GT|ypeInfo: %s\n", g_intern_static_string("GT|ypeInfo"));
+    printf ("GT+ypeInfo: %s\n", g_intern_static_string("GT+ypeInfo"));
+    printf ("GT_ypeInfo: %s\n", g_intern_static_string("GT_ypeInfo"));
+}
