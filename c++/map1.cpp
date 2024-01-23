25f21d710530752f78793654062592e83992a768
丁敬 <dingjing@live.cn>
2024-01-23 15:20:25 +0800

    zc

--- c++/map1.cpp
+++ c++/map1.cpp
@@ -13,13 +13,13 @@ void print_map(std::string_view comment, const std::map<std::string, int>& m)
 {
     std::cout << comment ;
     // iterate using C++17 facilities
-    for (const auto& [key, value] : m) {
-        std::cout << '[' << key << "] = " << value << "; ";
-    }
+//    for (const auto& [key, value] : m) {
+//        std::cout << '[' << key << "] = " << value << "; ";
+//    }
 // C++11 alternative:
-//  for (const auto& n : m) {
-//      std::cout << n.first << " = " << n.second << "; ";
-//  }
+  for (const auto& n : m) {
+      std::cout << n.first << " = " << n.second << "; ";
+  }
 // C++98 alternative
 //  for (std::map<std::string, int>::const_iterator it = m.begin(); it != m.end(); it++) {
 //      std::cout << it->first << " = " << it->second << "; ";
