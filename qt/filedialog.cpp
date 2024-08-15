baea0768dc0158cd9856dd84cf2f0df3c3c8ff14
丁敬 <dingjing@live.cn>
2024-08-15 11:39:49 +0800

    dialog

--- qt/filedialog.cpp
+++ qt/filedialog.cpp
@@ -4,6 +4,8 @@
 > Mail    : dingjing@live.cn
 > Created Time: Wed 31 May 2023 02:45:46 PM CST
  ************************************************************************/
+#include <QDebug>
+#include <QStringList>
 #include <QFileDialog>
 #include <QApplication>
 
@@ -12,6 +14,12 @@ int main (int argc, char* argv[])
     QApplication app(argc, argv);
 
     QFileDialog dlg;
+    dlg.setFileMode(QFileDialog::DirectoryOnly);
+    dlg.setOption(QFileDialog::ShowDirsOnly, true);
+
+    QObject::connect(&dlg, &QFileDialog::filesSelected, [=](const QStringList& selected) ->void {
+        qInfo() << selected;
+    });
 
     dlg.exec();
 
