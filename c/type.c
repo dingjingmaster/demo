9455baa5fc1164b52769ad3d2da92862557e3fc4
dingjing <dingjing@live.cn>
2024-10-16 14:13:17 +0800

    type

--- c/type.c
+++ c/type.c
@@ -9,7 +9,15 @@
 
 int main (int argc, char* argv[])
 {
+    printf ("uint8_t: %u\n", sizeof(uint8_t));
+    printf ("uint16_t: %u\n", sizeof(uint16_t));
+    printf ("uint32_t: %u\n", sizeof(uint32_t));
+    printf ("uint64_t: %u\n", sizeof(uint64_t));
+    printf ("unsigned char: %u\n", sizeof(unsigned char));
+    printf ("unsigned short: %u\n", sizeof(unsigned short));
+    printf ("unsigned int: %u\n", sizeof(unsigned int));
     printf ("unsigned long: %u\n", sizeof(unsigned long));
+    printf ("unsigned sort: %u\n", sizeof(unsigned short));
 
 
     return 0;
