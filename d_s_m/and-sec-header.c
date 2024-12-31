7aaeeb21093961800d85ce8671086e8e02e662fa
dingjing <dingjing@live.cn>
2024-12-31 15:48:10 +0800

    [FIX] dec

--- d_s_m/and-sec-header.c
+++ d_s_m/and-sec-header.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "and-sec-header.h"
 
 
 #define BOOL_STR(x) (x ? "true" : "false")
+
+#define MASTER_KEY "fa49c7b67bab98daa75f680990ac03b126d640495b900715" //"ZmE0OWM3YjY3YmFiOThkYWE3NWY2ODA5OTBhYzAzYjEyNmQ2NDA0OTViOTAwNzE1"
 
 
 int main (int argc, char* argv[])
@@ -23,6 +26,22 @@ int main (int argc, char* argv[])
     if (!res) {
         return -1;
     }
+
+#if 0
+    DrmFileHead drmFileHead;
+    FILE* fr = fopen(argv[1], "r");
+    if (fr) {
+        size_t readSize = fread(&drmFileHead, sizeof(drmFileHead), 1, fr);
+        printf("read size: %lu\n", readSize);
+        fclose(fr);
+    }
+#endif
+
+    char key[32] = {0};
+    memcpy(key, MASTER_KEY, sizeof(key));
+
+    int ret = DecryptSafeFile(argv[1], MASTER_KEY, ENCRYPT_ARITH_RC4);
+    printf("DecodeDRMHead: %d\n", ret);
 
     print_safe_head(&sh);
 
--- d_s_m/and-sec-header.h
+++ d_s_m/and-sec-header.h
@@ -405,61 +405,215 @@ struct __attribute__((packed)) _DrmFileHead
 C_TYPE_SIZE_CHECK(DrmFileHead, 4096);
 
 
-
-extern bool GetDRMFileId            (IN const uint8_t* fileName, OUT int8_t fileID);
-extern bool ReadDRMHead             (IN void* handle, OUT DrmFileHead* pFileHead, IN const uint8_t* masterKey);
-extern bool WriteDRMHead            (IN void* handle, IN DrmFileHead* pFileHead, IN const uint8_t* masterKey);
-extern bool GetPlainDRMDataKey      (INOUT void* pFileHead, INOUT uint8_t* dataKey, IN uint8_t dataKeyLen);
-extern bool GetDRMDataKey           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey, INOUT uint8_t* dataKey);
-extern bool ChgDRMMasterKey         (INOUT DrmFileHead* fileName, IN const uint8_t* oldMasterKey, IN uint8_t* newMaster);
-extern bool GetDRMMasterKey         (INOUT DrmFileHead* fileHead, IN const uint8_t* listKey, IN uint32_t listKeyNums, INOUT uint8_t* masterKey);
-extern bool DecodeDRMHead           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey);
-extern void EncodeDRMHead           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey);
-extern void MakeDRMHead             (INOUT DrmFileHead* fileHead, IN uint32_t fileType, IN uint32_t subType /*= 0*/, IN uint8_t* headBuffer/* = NULL*/, IN uint32_t headBufferSize /* = 0*/);
-extern bool ReadSafeFile            (IN const char* fileName, IN const SafeFileHead* fileHead, OUT void* dataBuffer, IN uint32_t dataBytes, IN int64_t offset /* = SAFE_HEAD_SIZE*/);
-extern bool WriteSafeFile           (IN const char* fileName, IN const SafeFileHead* fileHead, IN void* dataBuffer, IN uint32_t dataBytes, IN int64_t offset /*= SAFE_HEAD_SIZE*/);
-extern bool ReadSafeHead            (IN const char* fileName, OUT SafeFileHead* fileHead);
-extern bool WriteSafeHead           (IN const char* fileName, IN SafeFileHead* fileHead, IN bool update /*= false*/);
-extern bool DecodeSafeHead          (INOUT SafeFileHead* fileName, IN const uint8_t* masterKey/* = NULL*/);
-extern void EncodeSafeHead          (INOUT SafeFileHead* fileName, IN const uint8_t* masterKey/* = NULL*/);
-extern bool CheckSafeHead           (IN SafeFileHead* fileName, IN uint16_t fileType/* = FILE_TYPE_ANY*/);
-extern void MakeSafeHead            (INOUT SafeFileHead* fileHead, IN uint16_t fileType);
+extern void GenUUIDStr              (OUT uint8_t* buffer);
 extern uint32_t GetStrAdler32       (IN const uint8_t* szString);
+extern void InitCipher              (IN OUT CryptInfo *pCryptInfo);
 extern uint64_t Adler64             (IN const uint8_t* buffer, IN uint32_t len);
 extern uint32_t Adler32             (IN const uint8_t* buffer, IN uint32_t len);
 extern uint32_t Adler16             (IN const uint8_t* buffer, IN uint32_t len);
-extern void GenUUIDStr              (OUT uint8_t* buffer);
-extern uint32_t String2Key          (OUT char* keyArray, IN uint32_t keyArrayLen, IN uint8_t* inString);
-extern void Key2String              (OUT uint8_t* outString, IN char* keyArray, IN uint32_t keyArrayLen);
-extern void ExpandKey               (IN const char* inputBuffer, IN int64_t inputLen, OUT char* outputBuffer, IN int64_t outputLen);
 extern void GenRandKey              (OUT uint8_t* buffer, IN uint32_t bufferLen);
-extern bool DecryptSafeFileBuffer   (IN OUT uint8_t* fileBuffer, IN uint32_t len, IN uint64_t lnOffset, IN const uint8_t* dataKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
-extern bool EncryptSafeFileBuffer   (IN OUT uint8_t* fileBuffer, IN uint32_t len, IN uint64_t lnOffset, INOUT const uint8_t* dataKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
-extern void DecryptBuffer           (IN uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key, IN uint32_t keyLen, IN int arith);
-extern void EncryptBuffer           (IN uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key, IN uint32_t keyLen, IN int arith);
+extern bool GetDRMFileId            (IN const uint8_t* fileName, OUT int8_t fileID);
+extern void MakeSafeHead            (INOUT SafeFileHead* fileHead, IN uint16_t fileType);
+extern bool ReadSafeHead            (IN const char* fileName, OUT SafeFileHead* fileHead);
+extern bool DecodeDRMHead           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey);
+extern void EncodeDRMHead           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey);
+extern void DecodeDES               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
+extern void EncodeDES               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
+extern void DecodeSM4               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
+extern void EncodeSM4               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
+extern void DecodeDES3              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
+extern void EncodeDES3              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
 extern void EncodeXXTEA             (IN OUT uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key);
 extern void DecodeXXTEA             (IN OUT uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key);
 extern void EncodeTEA               (IN OUT uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key);
 extern void DecodeTEA               (IN OUT uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key);
-extern void DecodeAES               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen, IN uint8_t* IV /* = NULL*/, uint32_t iMode/* = ENCRYPT_MODE_ECB*/);
-extern void EncodeAES               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen, IN uint8_t* IV /* = NULL*/, uint32_t iMode/* = ENCRYPT_MODE_ECB*/);
+extern void DecodeSEAL              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
+extern void EncodeSEAL              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
+extern uint32_t String2Key          (OUT char* keyArray, IN uint32_t keyArrayLen, IN uint8_t* inString);
+extern void Key2String              (OUT uint8_t* outString, IN char* keyArray, IN uint32_t keyArrayLen);
+extern bool CheckSafeHead           (IN SafeFileHead* fileName, IN uint16_t fileType/* = FILE_TYPE_ANY*/);
+extern bool DecodeSafeHead          (INOUT SafeFileHead* fileName, IN const uint8_t* masterKey/* = NULL*/);
+extern void EncodeSafeHead          (INOUT SafeFileHead* fileName, IN const uint8_t* masterKey/* = NULL*/);
+extern bool GetPlainDRMDataKey      (INOUT void* pFileHead, INOUT uint8_t* dataKey, IN uint8_t dataKeyLen);
+extern bool WriteDRMHead            (IN void* handle, IN DrmFileHead* pFileHead, IN const uint8_t* masterKey);
+extern bool ReadDRMHead             (IN void* handle, OUT DrmFileHead* pFileHead, IN const uint8_t* masterKey);
+extern bool WriteSafeHead           (IN const char* fileName, IN SafeFileHead* fileHead, IN bool update /*= false*/);
+extern bool GetDRMDataKey           (INOUT DrmFileHead* fileHead, IN const uint8_t* masterKey, INOUT uint8_t* dataKey);
+extern bool ChgDRMMasterKey         (INOUT DrmFileHead* fileName, IN const uint8_t* oldMasterKey, IN uint8_t* newMaster);
 extern void DecodeRC4               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen);
 extern void EncodeRC4               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen);
 extern void DecodeENRC4             (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen);
 extern void EncodeENRC4             (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen);
-extern void DecodeSEAL              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
-extern void EncodeSEAL              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
-extern void InitCipher              (IN OUT CryptInfo *pCryptInfo);
-extern void DecodeDES               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
-extern void EncodeDES               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
-extern void DecodeDES3              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
-extern void EncodeDES3              (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key);
-extern void DecodeSM4               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
-extern void EncodeSM4               (IN uint8_t* pBuffer,IN uint32_t nLength, IN const uint8_t* Key);
+extern void ExpandKey               (IN const char* inputBuffer, IN int64_t inputLen, OUT char* outputBuffer, IN int64_t outputLen);
+extern void DecryptBuffer           (IN uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key, IN uint32_t keyLen, IN int arith);
+extern void EncryptBuffer           (IN uint8_t* buffer, IN uint32_t nLen, IN const uint8_t* key, IN uint32_t keyLen, IN int arith);
+extern bool GetDRMMasterKey         (INOUT DrmFileHead* fileHead, IN const uint8_t* listKey, IN uint32_t listKeyNums, INOUT uint8_t* masterKey);
+extern bool DecryptSafeFileBuffer   (IN OUT uint8_t* fileBuffer, IN uint32_t len, IN uint64_t lnOffset, IN const uint8_t* dataKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
+extern bool EncryptSafeFileBuffer   (IN OUT uint8_t* fileBuffer, IN uint32_t len, IN uint64_t lnOffset, INOUT const uint8_t* dataKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
+extern bool WriteSafeFile           (IN const char* fileName, IN const SafeFileHead* fileHead, IN void* dataBuffer, IN uint32_t dataBytes, IN int64_t offset /*= SAFE_HEAD_SIZE*/);
+extern bool ReadSafeFile            (IN const char* fileName, IN const SafeFileHead* fileHead, OUT void* dataBuffer, IN uint32_t dataBytes, IN int64_t offset /* = SAFE_HEAD_SIZE*/);
+extern void MakeDRMHead             (INOUT DrmFileHead* fileHead, IN uint32_t fileType, IN uint32_t subType /*= 0*/, IN uint8_t* headBuffer/* = NULL*/, IN uint32_t headBufferSize /* = 0*/);
+extern void DecodeAES               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen, IN uint8_t* IV /* = NULL*/, uint32_t iMode/* = ENCRYPT_MODE_ECB*/);
+extern void EncodeAES               (IN uint8_t* pBuffer, IN uint32_t nLength, IN const uint8_t* Key, IN uint32_t KeyLen, IN uint8_t* IV /* = NULL*/, uint32_t iMode/* = ENCRYPT_MODE_ECB*/);
 
-inline uint32_t GetSafeHeadAdler (IN SafeFileHead* fileHead)
+/**
+ * @brief 检查给定的文件是否为加密的文件
+ */
+extern bool MonIsEncryptFile        (IN const uint8_t* fileName);
+
+/**
+ * @brief 加密成符合动态加解密格式的密文
+ * @param fileName: 文件名
+ * @param masterKey: 加密主密钥,至少应为32字节，支持0x00-0xFF的全字符
+ * @param arith：加密算法,见常量ARITH_xxxx
+ *
+ * EDG密钥的输入方式是(支持多密钥):
+ *  1)直接传递密钥数据字符串:
+ *      如有两个加密密钥：AA3456789012345678901234567890AA和BB1234567890123456789012345678BB
+ *      则传递密钥为：“AA3456789012345678901234567890AABB1234567890123456789012345678BB”或“AA3456789012345678901234567890AA|BB1234567890123456789012345678BB”
+ *      其中前面的方式只适用于密钥长度为32个字符的情形，如果密钥长度不足32个字符，则只能用方式2，密钥之间用'|'进行分隔，如|1233243242342ABC|EDGF34324234234|等
+ *  2)通过加入|KEYFLAG_EDG|标记传递:
+ *      密钥同上，传递的密钥数据字符串为：“|KEYFLAG_EDG|AA3456789012345678901234567890AA|BB1234567890123456789012345678BB|”，其中最后一个'|'分隔符也可以没有
+ *
+ * 兼容第三方密钥的输入方式是(（支持多密钥）:
+ * 为兼容第三方，在有第三方密文的前提下，密钥的输入方式是:
+ * 加密成EDG文件格式:
+ *  |KEYFLAG_EDG|Monsafe的加密密钥|KEYFLAG_CDG8|第三方密钥1|第三方密钥2|...
+ * 加密成第三方文件格式：
+ *  |KEYFLAG_CDG8|第三方的加密密钥|第三方密钥2|第三方密钥3|...
+ *
+ * 其中:
+ *  第一个KEYFLAG_用于标记加密的时候是加密成EDG文件格式(标记为KEYFLAG_EDG)还是第三方文件格式(标记为KEYFLAG_CDG8等第三方标记)，用于兼容第三方密文
+ *  |KEYFLAG_EDG|为Monsafe的密钥标识,由调用程序自己加入，如果要加密为第三方的密文模式，则第一个密钥标记应为第三方的密钥标记
+ *  |KEYFLAG_CDG8|为第三方的密钥标识，不同的第三方标识不同,由用户设置密钥时候输入
+ *
+ * 例如加密成EDG密文格式:
+ *  MonSafe密钥为: 1234567890abcdefgh
+ *  1) 没有第三方密钥: 调用程序直接传递该密钥(因Monsafe密钥为32个字符，因此传递给参数时，要存放到空间>=32字节中，不足部分用0补足)或者传递|KEYFLAG_EDG|1234567890abcdefgh| (这种格式如果密钥长度不够32个字符，则不需要补足)
+ *  2) 如果用户输入第三方密钥|KEYFLAG_IPG|ABCDEFGHIJKLMNOP12345678|0987654321CEDG| 
+ *  则调用程序应将这两类密钥组合为:
+ *      |KEYFLAG_EDG|1234567890abcdefgh|KEYFLAG_IPG|ABCDEFGHIJKLMNOP12345678|0987654321CEDG|
+ *      然后再传递组合后的密钥(Monsafe的密钥不足32字符不需要特殊处理，库文件会自动补足)
+ *
+ * 例如加密成CDG8密文格式(只有组合密钥，没有独立密钥):
+ *  A. CDG8只有单密钥: 1234567890abcdefgh123456
+ *      调用程序的组合密钥为: |KEYFLAG_CDG8|1234567890abcdefgh123456| 然后再传递组合后的密钥
+ *  B. CDG8有多密钥，其中加密密钥为1234567890abcdefgh123456，其它密钥为：|ABCDEFGHIJKLMNOP12345678|0987654321CEDG|
+ *      调用程序的组合密钥为:
+ *          |KEYFLAG_CDG8|1234567890abcdefgh123456|ABCDEFGHIJKLMNOP12345678|0987654321CEDG|
+ *      其中加密密钥1234567890abcdefgh123456必须为第一个
+ *
+ * @return
+ *      -18: 获取密钥错误
+ *      -14: 获取多密钥错误
+ *      -13: 文件名太长
+ *      -12: 坏文件
+ *      -11: 校验不通过
+ *      -10: 文件大小错误
+ *      -9: 移动指针错误
+ *      -8: 数据错误
+ *      -7: 内存分配错误
+ *      -6: 无法加密文件
+ *      -5: 无法写文件
+ *      -4: 无法读文件
+ *      -3: 无法创建新文件
+ *      -2: 无法打开源文件
+ *      -1: 获取密钥错误
+ *      <0: 错误码
+ *      0： 加密失败
+ *      1： 加密成功
+ *      2： 文件已经加密（文件本身是密文）
+ */
+extern int EncryptSafeFile          (IN const char* fileName, IN const char* masterKey, IN int arith/* = ENCRYPT_ARITH_RC4 */);
+
+/**
+ * @brief 加密成符合动态加解密格式的密文
+ * @param pPlainFileName：待加密的源文件全名
+ * @param pCipherFileName: 加密后的文件全名
+ * @param masterKey: 加密主密钥,至少应为32字节，支持0x00-0xFF的全字符。定义方法：UCHAR Key[32] ={0}; 可定义为32个字符，方便以后扩展到256位密钥
+ * @param arith: 加密算法,见常量ENCRYPT_ARITH__xxxx
+ *
+ * @return
+ *      -18: 获取密钥错误
+ *      -14: 获取多密钥错误
+ *      -13: 文件名太长
+ *      -12: 坏文件
+ *      -11: 校验不通过
+ *      -10: 文件大小错误
+ *      -9: 移动指针错误
+ *      -8: 数据错误
+ *      -7: 内存分配错误
+ *      -6: 无法加密文件
+ *      -5: 无法写文件
+ *      -4: 无法读文件
+ *      -3: 无法创建新文件
+ *      -2: 无法打开源文件
+ *      -1: 获取密钥错误
+ *      <0: 错误码
+ *      0： 加密失败
+ *      1： 加密成功
+ *      2： 文件已经加密（文件本身是密文）
+ */
+extern int EncryptSafeFile2         (IN const char* pPlainFileName, IN const char* pCipherFileName, IN const uint8_t* masterKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
+
+/**
+ * @return
+ *      -18: 获取密钥错误
+ *      -14: 获取多密钥错误
+ *      -13: 文件名太长
+ *      -12: 坏文件
+ *      -11: 校验不通过
+ *      -10: 文件大小错误
+ *      -9: 移动指针错误
+ *      -8: 数据错误
+ *      -7: 内存分配错误
+ *      -6: 无法加密文件
+ *      -5: 无法写文件
+ *      -4: 无法读文件
+ *      -3: 无法创建新文件
+ *      -2: 无法打开源文件
+ *      -1: 密钥错误
+ *      0： 加密失败
+ *      1： 加密成功
+ *      2： 文件已经加密（文件本身是密文）
+ */
+extern int DecryptSafeFile          (IN const char* pFileName, IN  const uint8_t* masterKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
+
+/**
+ * @return
+ *      -18: 获取密钥错误
+ *      -14: 获取多密钥错误
+ *      -13: 文件名太长
+ *      -12: 坏文件
+ *      -11: 校验不通过
+ *      -10: 文件大小错误
+ *      -9: 移动指针错误
+ *      -8: 数据错误
+ *      -7: 内存分配错误
+ *      -6: 无法加密文件
+ *      -5: 无法写文件
+ *      -4: 无法读文件
+ *      -3: 无法创建新文件
+ *      -2: 无法打开源文件
+ *      -1: 密钥错误
+ *      <0: 错误码
+ *      0： 加密失败
+ *      1： 加密成功
+ *      2： 文件已经加密(文件本身是密文)
+ */
+extern int DecryptSafeFile2         (IN const char* pCipherFileName, IN const char* pPlainFileName, IN  const uint8_t* masterKey, IN int arith/* = ENCRYPT_ARITH_RC4*/);
+
+static inline uint32_t GetSafeHeadAdler (IN SafeFileHead* fileHead)
 {
     return Adler32((uint8_t*) &fileHead->magic, C_STRUCT_OFFSET_OF(SafeFileHead, headAdler));
+}
+
+/**
+ * @brief 检查给定的文件头内容是否为加密的文件
+ */
+static inline bool MonChechEncrypt (IN SafeFileHead* fileHead)
+{
+    return CheckSafeHead(fileHead, FILE_TYPE_TDE);
 }
 
 static inline void print_safe_head (SafeFileHead* sh)
