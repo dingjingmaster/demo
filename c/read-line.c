55578a661b1d0c3144cd2b8e1fce64ad393b486f
dingjing <dingjing@live.cn>
2025-01-03 10:37:18 +0800

    [ADD] read line

---
+++ c/read-line.c
@@ -0,0 +1,101 @@
+/*************************************************************************
+> FileName: read-line.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Fri Jan  3 09:27:00 2025
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char* read_line (FILE* fr);
+
+int main (int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf ("请输入要读取的文件\n");
+        return -1;
+    }
+
+    FILE* fr = fopen (argv[1], "r");
+    char* bufLine = NULL;
+    while ((bufLine = read_line(fr)) != NULL) {
+        //printf ("line: '%s'\n", bufLine);
+        printf ("%s\n", bufLine);
+        free (bufLine);
+        bufLine = NULL;
+    }
+    fclose(fr);
+
+    if (bufLine) {
+        free (bufLine);
+    }
+
+    return 0;
+}
+
+static inline char* calloc_and_memcpy(const char* oldStr, int64_t oldLen, const char* addStr, int64_t addLen)
+{
+    int64_t newLineLen = oldLen + addLen;
+    char* tmp = (char*) malloc (newLineLen + 1);
+    if (!tmp) {
+        return NULL;
+    }
+
+    memset(tmp, 0, newLineLen + 1);
+    if (oldStr) {
+        memcpy(tmp, oldStr, oldLen);
+    }
+    memcpy(tmp + oldLen, addStr, addLen);
+
+    return tmp;
+}
+
+char* read_line (FILE* fr)
+{
+    if (!fr) {
+        printf("fopen error\n");
+        return NULL;
+    }
+    int64_t cur = ftell(fr);
+
+    char* res = NULL;
+    int64_t lineLen = 0;
+
+    char buf[10] = {0};
+    while (true) {
+        memset(buf, 0, sizeof(buf));
+        int size = fread(buf, 1, sizeof(buf) - 1, fr);
+        if (size <= 0) {
+            break;
+        }
+
+        // 截取一行
+        int i = 0;
+        bool find = false;
+        for (i = 0; i < size; ++i) {
+            if (buf[i] == '\n') {
+                // 找到
+                char* tmp = calloc_and_memcpy(res, lineLen, buf, i);
+                if (res) { free (res); res = NULL; }
+                if (!tmp) { find = true; printf("impossible\n"); break; }           // impossible
+                res = tmp;
+                lineLen += i;
+                find = true;
+                break;
+            }
+        }
+
+        if (find) { fseek(fr, cur + lineLen + 1, SEEK_SET); break; }
+
+        // 分配内存, 一行的长度大于 buf
+        char* tmp = calloc_and_memcpy(res, lineLen, buf, size);
+        if (res) { free(res); res = NULL; }
+        if (!tmp) { find = true; printf("impossible\n"); break; } // impossible
+        res = tmp;
+        lineLen += size;
+    }
+
+    return res;
+}
