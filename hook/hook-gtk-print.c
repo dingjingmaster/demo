5dee8a2137aa2b3d341fe2ac06170935ad150f35
dingjing <dingjing@live.cn>
2025-05-29 17:29:40 +0800

    [add] gtk print

--- hook/hook-gtk-print.c
+++ hook/hook-gtk-print.c
@@ -6,15 +6,39 @@
  ************************************************************************/
 #define _GNU_SOURCE
 #include <dlfcn.h>
+#include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
 
-typedef void (*GtkBeginPrintCB) (void* o, void* h, void* n);
-
+static bool isGedit = false;
 static void* gtkPrintHandle = NULL;
-GtkBeginPrintCB gtkBeginPrintCB = NULL;
+
+
+typedef void (*GeditBeginPrintCB) (void* op, void* context, void* job);
+static GeditBeginPrintCB geditBeginPrintCB = NULL;
+static void gedit_begin_print_cb (void* operation, void* context, void* job)
+{
+    char file[4096] = {0};
+
+}
+
+
+static void get_program_full_path (char buf[], int bufLen)
+{
+        char pBuf[128] = {0};
+
+        if (snprintf(pBuf, sizeof(pBuf) - 1, "/proc/%d/exe", getpid()) > 0) {
+                if (readlink(pBuf, buf, bufLen - 1) < 0) {
+                        syslog(LOG_ERR, "readlink error: %s", strerror(errno));
+                }
+        }
+}
 
 
 typedef void* (*GtkPrintOperationNew)();
@@ -36,6 +60,13 @@ unsigned long g_signal_connect_data (void* ins, const char* detail, void* cb, vo
     if (pp) {
         if ((ins == gtkPrintHandle) && (!strcmp(detail, "begin-print"))) {
             syslog(LOG_ERR, "begin-print");
+            char proc[4096] = {0};
+            get_program_full_path(proc, sizeof(proc) - 1);
+            if (strstr(proc, "/gedit")) {
+                isGedit = true;
+                geditBeginPrintCB = cb;
+                return pp (ins, detail, gedit_begin_print_cb, data, destroyData, flags);
+            }
             return 0;
         }
         return pp (ins, detail, cb, data, destroyData, flags);
