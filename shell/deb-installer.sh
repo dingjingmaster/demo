8e22d179079753d7cce2c0cda21a9cbdaa850ff9
dingjing <dingjing@live.cn>
2025-08-21 18:49:44 +0800

    deb 安装脚本，增加机器架构判断

--- shell/deb-installer.sh
+++ shell/deb-installer.sh
@@ -2,17 +2,33 @@
 
 set -e
 
-
 targetVer=""
 targetPkg=""
+targetArch="$(uname -m)"
 curDir=$(realpath -- $(dirname -- $0))
 
+
+if [ "x86_64" == "$targetArch" ]; then
+	targetArch="x86"
+elif [ "X86_64" == "$targetArch" ]; then
+	targetArch="x86"
+elif [ "aarch" == "$targetArch" ]; then
+	targetArch="arm"
+elif [ "aarch64" == "$targetArch" ]; then
+	targetArch="arm"
+elif [ "arm64" == "$targetArch" ]; then
+	targetArch="arm"
+fi
 
 while IFS= read -r file; do 
 	fname="${file##*/}"
 	name1="${fname%.*}"
 	name2="${name1%_*}"
+	arch=$(echo $name1 | awk -F'_' '{print $2}')
 	ver=$(echo $name2 | awk -F'-' '{print $2}')
+	if [ "$arch" != $targetArch ]; then
+		continue
+	fi
 	if [ "" == "$targetPkg" ]; then
 		targetPkg="$file"
 		targetVer="$ver"
@@ -24,7 +40,9 @@ while IFS= read -r file; do
 	fi
 done < <(find $curDir -maxdepth 1 -type f -name "andbox-*.deb")
 
-echo "找到最新安装包: $targetPkg, 版本号: $targetVer"
+echo "包路径: $targetPkg"
+echo "  版本: $targetVer"
+echo "  架构: $targetArch"
 
 echo "开始安装..."
 pkexec dpkg -i $targetPkg
